371473f0bb02e3a219801959142c25eb0a163336
programmer-abdullah-qr <abdullahqadeer711@gmail.com>
2026-07-06 18:55:02 +0500

    Create a Simple Introduction Program.

---
+++ problem15.py
@@ -0,0 +1,9 @@
+Name = input("Enter a Name :")
+Age = int(input("Enter a Age :"))
+print ("hello",Name,"!")
+print("you are",Age,"year old")
+print ("Next year you will be",Age+1,"year old")
+if(Age>=18):
+    print("You are eligible to vote.")
+else:
+    print("You are Not eligible to vote.")
