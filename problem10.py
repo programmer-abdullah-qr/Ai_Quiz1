f4c75a198c9ffc0f2c50e2b2f9f9208a4baa7e47
programmer-abdullah-qr <abdullahqadeer711@gmail.com>
2026-07-06 17:49:02 +0500

    Cheak if else codition

---
+++ problem10.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a first digit :"))
+num2 = int(input("Enter a Second digit :"))
+if(num1 >= num2 ):
+    print("num1 is greater number")
+else:
+    print("num2 is greater number")    
