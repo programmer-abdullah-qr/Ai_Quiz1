be91ec8ae8e0d53fd04a91f4a06dbf0a08f92883
programmer-abdullah-qr <abdullahqadeer711@gmail.com>
2026-07-06 18:43:54 +0500

    Check whether the number is divisible by both 5 and 7.

---
+++ problem14.py
@@ -0,0 +1,5 @@
+Number = int(input("Enter a digit :"))
+if(Number%5==0 and Number%7==0):
+    print("The number is divisible by both 5 and 7")
+else:
+    print("The number is NOT divisible by both 5 and 7")
