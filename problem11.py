725b2f80c6341e221158dac739aca589220a0a7e
programmer-abdullah-qr <abdullahqadeer711@gmail.com>
2026-07-06 18:07:44 +0500

    Create a Grade Calculator

---
+++ problem11.py
@@ -0,0 +1,13 @@
+Marks = int (input("Enter a Marks:"))
+if(Marks>=90 and Marks<=100):
+    print("Grade = A+")
+elif(Marks>=80 and Marks <= 89):
+    print("Grade = A")
+elif(Marks>=70 and Marks <= 79):
+    print("Grade = B")
+elif(Marks>=60 and Marks <= 69):
+    print("Grade = c")
+elif(Marks>=101 ):
+    print("Enter a wrong marks")
+else:
+    print("Grade = Fail")
